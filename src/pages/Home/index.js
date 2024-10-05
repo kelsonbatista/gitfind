1873cb8526a5b5a526e574a3598849b333b5cf45
Kelson <kelsonbatista@gmail.com>
2024-10-05 16:23:02 -0300

    Page part 5 api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import avatar from '../../assets/images/avatar.png';
 import background from '../../assets/images/background.jpg';
 import Header from '../../components/Header';
@@ -5,6 +6,26 @@ import ItemList from '../../components/ItemList';
 import './styles.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repositories, setRepositories] = useState([]);
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${username}`)
+      .then((response) => response.json())
+      .then((data) => data);
+    
+    console.log(userData);
+
+    const repoData = await fetch(`https://api.github.com/users/${username}/repos`)
+      .then((response) => response.json())
+      .then((data) => data);
+  }
+
   return (
     <div className="App">
       <Header />
@@ -12,8 +33,13 @@ function App() {
         <img src={background} alt="Background" className="background" />
         <div className="info">
           <div className="search">
-            <input name="username" type="text" placeholder="@username" />
-            <button>Find</button>
+            <input
+              name="username"
+              type="text"
+              placeholder="@username"
+              value={username}
+              onChange={handleUsername} />
+            <button onClick={handleGetData}>Find</button>
           </div>
           <div className="profile">
             <img src={avatar} className="avatar" alt="Avatar" />
@@ -26,9 +52,13 @@ function App() {
           <hr />
           <div>
             <h4 className="repository">Repositories</h4>
-            <ItemList title="Primeiro item" description="Description" />
-            <ItemList title="Primeiro item" description="Description" />
-            <ItemList title="Primeiro item" description="Description" />
+            {repositories.map((repository) => (
+              <ItemList
+                key={repository.id}
+                title={repository.name}
+                description={repository.description}
+              />
+            ))}
           </div>
         </div>
       </div>
