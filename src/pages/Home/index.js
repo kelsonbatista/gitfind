3b04dbfa9fea2edf56b6bfa209c8d278bd574fe8
Kelson <kelsonbatista@gmail.com>
2024-10-05 14:20:25 -0300

    Page part 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,20 @@
+import background from '../../assets/images/background.jpg';
 import Header from '../../components/Header';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <img src={background} alt="Background" className="background" />
+        <div className="info">
+          <div>
+            <input name="username" type="text" placeholder="@username" />
+            <button>Find</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
