08ff018a5c442b265ade5b7d3616e3b10eda4318
Kelson <kelsonbatista@gmail.com>
2024-10-05 15:01:05 -0300

    Page part 3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import avatar from '../../assets/images/avatar.png';
 import background from '../../assets/images/background.jpg';
 import Header from '../../components/Header';
 import './styles.css';
@@ -9,9 +10,32 @@ function App() {
       <div className="content">
         <img src={background} alt="Background" className="background" />
         <div className="info">
-          <div>
+          <div className="search">
             <input name="username" type="text" placeholder="@username" />
             <button>Find</button>
+          </div>
+          <div className="profile">
+            <img src={avatar} className="avatar" alt="Avatar" />
+            <div>
+              <h3>Username</h3>
+              <span>@username</span>
+              <p>Description</p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h3>Repositories</h3>
+            <ul>
+              <li>
+                <a href="/#">Repository 1</a>
+              </li>
+              <li>
+                <a href="/#">Repository 2</a>
+              </li>
+              <li>
+                <a href="/#">Repository 3</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
